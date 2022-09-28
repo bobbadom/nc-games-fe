9480892091ea756a921e382174a638f2acebfa9b
bobbadom <robertm.denton@btinternet.com>
2022-09-28 08:48:22 +0100

    finish task 7 with optemistic rendering

--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -15,15 +15,38 @@ const {review_id}= useParams()
         })
     },[review_id])
 
-  return (
-    <section>
+    const voteOnReview = (() =>{
+
+
+        setSingleReview((currentReview)=>{
+            return {...singleReview, votes: singleReview.votes + 1}
+        })     
+        const voteChange= {
+          inc_vote : 1,
+        }
+        axios.patch(`https://bobs-brilliant-backend-project.herokuapp.com/api/reviews/${review_id}`, voteChange)
+        .then(({data})=>{
+         
+        })
+        .catch((err)=>{
+          setSingleReview((currentReview)=>{
+            if(currentReview.review_id === review_id){
+              return {...currentReview, votes: currentReview.votes - 1}
+            }
+            return currentReview
+          })
+        })
+      })
+        
+        return (
+          <section>
 <h2>  {singleReview.title} </h2>
-<img src={singleReview.review_img_url}/>
+<img src={singleReview.review_img_url} alt={singleReview.title}/>
 <h3>{singleReview.owner}</h3>
 <p><Link to ={`/category/${singleReview.category}`}><strong> Category</strong>
     &nbsp;:&nbsp;{singleReview.category}</Link></p>
 <p>{singleReview.review_body}</p>
-<p>{singleReview.votes}</p>
+<button onClick={voteOnReview} >{singleReview.votes} votes</button>
     </section>
   )
 }
