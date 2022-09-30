b4a2c526eb82fc469d755409e1b0a3bf3de0a887
bobbadom <robertm.denton@btinternet.com>
2022-09-30 16:42:21 +0100

    add use context

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -1,11 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
 import axios from 'axios'
-
+import { UserContext } from '../context/User'
  const CommentAdder = ({comments, setComments, review_id}) => {
-const [username, setUsername] = useState('')
 const [textBody, setTextBody] = useState('')
 const [posted, setPosted] = useState(false)
-
+const {loggedInUser} =useContext(UserContext)
 
 
 
@@ -14,16 +13,14 @@ e.preventDefault()
 axios.post(`https://bobs-brilliant-backend-project.herokuapp.com/api/reviews/${review_id}/comments`,{votes : 0, 
 review_id: review_id,
  created_at:new Date(),
-  username: username, 
+  username: loggedInUser.username, 
   body : textBody, 
   comment_id : ((comments.length !== 0) ? ((comments[comments.length-1].comment_id)+1) : 1)})
 .then(({data})=>{
   setComments((currComments) => {
-      console.log(currComments, 'comments')
       setPosted(true)
       return [ ...currComments, data.comment]
     })
-    setUsername('')
     setTextBody('')
   })
 }
@@ -39,10 +36,7 @@ if(posted){
   return (
     <section>
 <form onSubmit={(e) => handleSubmit(e)}>
-<label htmlFor='username' ><strong>Username</strong></label>
-<br></br>
-<input value={username}id='username'type="text" placeholder='JoeBloggs' required onChange={(e) => setUsername(e.target.value)}/>
-<br></br>
+
 <label htmlFor='comment'></label>
 <br></br>
 <textarea value ={textBody}id='comment' rows="10" cols="40" placeholder='This game ia amazing' required  onChange={(e) => setTextBody(e.target.value)}></textarea>
